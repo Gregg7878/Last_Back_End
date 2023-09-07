364f0de1cf7e1052ba143afcc994526fb2e31861
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-09-07 09:18:06 +0300

    added Controller

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,51 @@
+class ActivitiesController < ApplicationController
+    before_action :authenticate_user
+    before_action :set_activity, only: [:show, :update, :destroy]
+  
+    def index
+      @activities = current_user.activities
+      render json: @activities, status: :ok
+    end
+  
+ 
+    def show
+      render json: @activity, status: :ok
+    end
+  
+    def create
+      @activity = current_user.activities.build(activity_params)
+  
+      if @activity.save
+        render json: @activity, status: :created
+      else
+        render json: { errors: @activity.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @activity.update(activity_params)
+        render json: @activity, status: :ok
+      else
+        render json: { errors: @activity.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+
+    def destroy
+      @activity.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def set_activity
+      @activity = current_user.activities.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { message: 'Activity not found' }, status: :not_found
+    end
+  
+    def activity_params
+      params.permit(:name, :date, :description)
+    end
+  end
+  
